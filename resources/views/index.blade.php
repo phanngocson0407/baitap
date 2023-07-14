2cf7012cf2cb1ffaea55fc8d813734c927e50eb0
phanngocson0407 <sonfpt149@gmail.com>
2023-07-14 17:15:21 +0700

    Son cập nhật

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
 
 <!-- Featured Start -->
 <div class="container-fluid pt-5">
+    <h1 class="fa fa-check text-primary m-0 mr-3">Phan Ngoc Son</h1>
     <div class="row px-xl-5 pb-3">
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center border mb-4" style="padding: 30px;">
