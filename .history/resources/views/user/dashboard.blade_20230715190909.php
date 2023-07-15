5bcf3ae93c8ad61eca9d5ce4f4b99ffe871ae723
PhuTran <pha081201@gmail.com>
2023-07-15 19:12:41 +0700

    a

--- .history/resources/views/user/dashboard.blade_20230715190909.php
+++ .history/resources/views/user/dashboard.blade_20230715190909.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>SHOP GIÀY NEW</title>
+    <title>Shop Giày New</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
@@ -37,7 +37,7 @@
             <div class="col-lg-3 d-none d-lg-block">
                 <a href="{{URL('/')}}" class="text-decoration-none">
                     <h1 class="m-0 display-5 font-weight-semi-bold"><span
-                            class="text-primary font-weight-bold border px-3 mr-1">GIÀY</span>  NEW</h1>
+                            class="text-primary font-weight-bold border px-3 mr-1">S</span>Shopper</h1>
                 </a>
             </div>
             <div class="col-lg-6 col-6 text-left">
--- .history/resources/views/user/dashboard.blade_20230715190946.php
+++ .history/resources/views/user/dashboard.blade_20230715190946.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>SHOP GIÀY NEW</title>
+    <title>Shop Giày New</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
