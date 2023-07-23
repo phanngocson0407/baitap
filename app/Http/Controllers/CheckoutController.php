f1b95e812acc601055d37be2c11ee5a1711a4a23
phanngocson0407 <sonfpt149@gmail.com>
2023-07-23 23:56:53 +0700

    dasd

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -10,75 +10,77 @@ class CheckoutController extends Controller
         $data= $request->all();
         $code_Cart = rand(00000,99999);
         $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+
 // $vnp_Returnurl = "http://giaynew.demo.com/xacnhanvnpay";
 $vnp_Returnurl = "http://giaynew.demo.com/xacnhanvnpay";
-//$vnp_Returnurl = "http://127.0.0.1:8000/xacnhanvnpay";
+// $vnp_Returnurl = "http://127.0.0.1:8000/xacnhanvnpay";
 $vnp_TmnCode = "BR8YFTUY";//Mã website tại VNPAY 
 $vnp_HashSecret = "OSAKVFQBCMPXGYJBVGNPEJENHSQUSLIA"; //Chuỗi bí mật
 
-$vnp_TxnRef = $code_Cart; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
-$vnp_OrderInfo = 'Test thanh toan';
-$vnp_OrderType ='billpayment';
-$vnp_Amount = $data['totalvnpay'] * 100;
-$vnp_Locale = 'vn';
-$vnp_BankCode =  'NCB';
-$vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
- 
-$inputData = array(
-    "vnp_Version" => "2.1.0",
-    "vnp_TmnCode" => $vnp_TmnCode,
-    "vnp_Amount" => $vnp_Amount,
-    "vnp_Command" => "pay",
-    "vnp_CreateDate" => date('YmdHis'),
-    "vnp_CurrCode" => "VND",
-    "vnp_IpAddr" => $vnp_IpAddr,
-    "vnp_Locale" => $vnp_Locale,
-    "vnp_OrderInfo" => $vnp_OrderInfo,
-    "vnp_OrderType" => $vnp_OrderType,
-    "vnp_ReturnUrl" => $vnp_Returnurl,
-    "vnp_TxnRef" => $vnp_TxnRef
-    // "vnp_ExpireDate"=>$vnp_ExpireDate
+
+        $vnp_TxnRef = $code_Cart; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
+        $vnp_OrderInfo = 'Test thanh toan';
+        $vnp_OrderType ='billpayment';
+        $vnp_Amount = $data['totalvnpay'] * 100;
+        $vnp_Locale = 'vn';
+        $vnp_BankCode =  'NCB';
+        $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
+                
+                $inputData = array(
+            "vnp_Version" => "2.1.0",
+            "vnp_TmnCode" => $vnp_TmnCode,
+            "vnp_Amount" => $vnp_Amount,
+            "vnp_Command" => "pay",
+            "vnp_CreateDate" => date('YmdHis'),
+            "vnp_CurrCode" => "VND",
+            "vnp_IpAddr" => $vnp_IpAddr,
+            "vnp_Locale" => $vnp_Locale,
+            "vnp_OrderInfo" => $vnp_OrderInfo,
+            "vnp_OrderType" => $vnp_OrderType,
+            "vnp_ReturnUrl" => $vnp_Returnurl,
+            "vnp_TxnRef" => $vnp_TxnRef
+            // "vnp_ExpireDate"=>$vnp_ExpireDate
     
-);
+        );
 
-if (isset($vnp_BankCode) && $vnp_BankCode != "") {
-    $inputData['vnp_BankCode'] = $vnp_BankCode;
-}
-if (isset($vnp_Bill_State) && $vnp_Bill_State != "") {
-    $inputData['vnp_Bill_State'] = $vnp_Bill_State;
-}
+        if (isset($vnp_BankCode) && $vnp_BankCode != "") {
+            $inputData['vnp_BankCode'] = $vnp_BankCode;
+        }
+        if (isset($vnp_Bill_State) && $vnp_Bill_State != "") {
+            $inputData['vnp_Bill_State'] = $vnp_Bill_State;
+        }
 
-//var_dump($inputData);
-ksort($inputData);
-$query = "";
-$i = 0;
-$hashdata = "";
-foreach ($inputData as $key => $value) {
-    if ($i == 1) {
-        $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
-    } else {
-        $hashdata .= urlencode($key) . "=" . urlencode($value);
-        $i = 1;
-    }
-    $query .= urlencode($key) . "=" . urlencode($value) . '&';
-}
+        //var_dump($inputData);
+        ksort($inputData);
+        $query = "";
+        $i = 0;
+        $hashdata = "";
+        foreach ($inputData as $key => $value) {
+            if ($i == 1) {
+                $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+            } else {
+                $hashdata .= urlencode($key) . "=" . urlencode($value);
+                $i = 1;
+            }
+            $query .= urlencode($key) . "=" . urlencode($value) . '&';
+        }
 
-$vnp_Url = $vnp_Url . "?" . $query;
-if (isset($vnp_HashSecret)) {
-    $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret);//  
-    $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
-}
-$returnData = array('code' => '00'
-    , 'message' => 'success'
-    , 'data' => $vnp_Url);
-    if (isset($_POST['redirect'])) {
-        header('Location: ' . $vnp_Url);
-        die();
-    } else {
-        echo json_encode($returnData);
+        $vnp_Url = $vnp_Url . "?" . $query;
+        if (isset($vnp_HashSecret)) {
+            $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret);//  
+            $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+        }
+            $returnData = array('code' => '00'
+        , 'message' => 'success'
+        , 'data' => $vnp_Url);
+        if (isset($_POST['redirect'])) {
+            header('Location: ' . $vnp_Url);
+            die();
+        } else {
+            echo json_encode($returnData);
+        }
     }
-    }
-    function execPostRequest($url, $data)
+    public function execPostRequest($url, $data)
     {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -97,18 +99,16 @@ $returnData = array('code' => '00'
         return $result;
     }
     public function momopayment(Request $request){
-        
+        $data= $request->all();
         $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
         $partnerCode = 'MOMOBKUN20180529';
         $accessKey = 'klm05TvNBzhg7h7j';
         $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
-        $orderInfo = "Thanh toán qua MoMo";
-        $amount = "10000";
+        $orderInfo = "Thanh toann qua MoMo";
+        $amount = $data['totalmomo']*100;
         $orderId = time() . "";
-        // $redirectUrl = "http://127.0.0.1:8000/xacnhanmomo";
-        // $ipnUrl = "http://127.0.0.1:8000/xacnhanmomo";
-        $redirectUrl = "http://giaynew.demo.com/xacnhanmomo";
-        $ipnUrl = "http://giaynew.demo.com/xacnhanmomo";
+        $redirectUrl = "http://127.0.0.1:8000/xacnhanmomo";
+        $ipnUrl = "http://127.0.0.1:8000/xacnhanmomo";
         $extraData = "";
         $requestId = time() . "";
         $requestType = "payWithATM";
